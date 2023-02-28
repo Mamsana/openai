7138d369d1038d18f35a9dc3810340f0d1318547
Mamsana <alicemamsana@gmail.com>
2023-02-28 16:51:32 +0300

    updates

--- live.py
+++ live.py
@@ -1,25 +1,22 @@
 import requests
 import os
 import openai
-import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = os.environ["OPENAI_API_KEY"]
 
 url = "https://whisper.lablab.ai/asr"
 
 payload = {}
 files = [ ('audio_file',('audio.mp3',open('audio.mp3','rb'),'audio/mpeg')) ]
 response = requests.request("POST", url, data=payload, files=files)
-prompt = response.json()["text"]
-print("The following statement is from your audio:\n{}".format(prompt))
+text = response.json()["text"]
+print("The following message is from your audio:{}".format(text))
 
-text = prompt
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt="Please generate a high quality and detailed image prompt for dall-e 2 based on the following: '" + text + "'.",
   temperature=0.7,
-  max_tokens=2500,
+  max_tokens=1000,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
@@ -27,21 +24,10 @@ response = openai.Completion.create(
 prompt = response.choices[0].text
 print("\nBased on the above statement, you got this prompt:{}".format(prompt))
 
-url = "https://api.openai.com/v1/images/generations"
-prompt = prompt
-model = "image-alpha-001"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {openai.api_key}"
-}
-data = {
-    "model": model,
-    "prompt": prompt,
-    "num_images": 1,
-    "size": "256x256",
-    "response_format": "url"
-}
-response = requests.post(url, headers=headers, json=data)
-response_data = response.json().get("data", [])
-image_url = response_data[0].get("url")
+response = openai.Image.create(
+  prompt=prompt,
+  n=1,
+  size="512x512"
+)
+image_url = response['data'][0]['url']
 print ("\nThe generated image is at: {}".format(image_url))
